a2d45c8133ba64c5a3d164a132ab6d36fcee122e
gauchansujal <151667820+gauchansujal@users.noreply.github.com>
2025-12-23 08:42:17 +0545

    added

--- src/interfaces/user.repository.interface.ts
+++
@@ -1,6 +0,0 @@
-import { UserDocument, User } from "../types/user.type"
-
-export interface UserRepositoryInterface {
-    createUser(user: User): Promise<UserDocument>;
-    getUsers(): Promise<UserDocument[]>;
-}
